8a7d0affb116c2a79cc12b055d357a40fca85290
FU <aderx@qq.com>
2021-09-12 23:47:03 +0800

    #add eslint增加ts规则支持

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,23 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
+    "parser": '@typescript-eslint/parser',
+    "extends": [
+        "plugin:react/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
     "parserOptions": {
         "ecmaFeatures": {
-            "jsx": true
+            "jsx": true,
+            "tsx": true,
         },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        '@typescript-eslint',
     ],
     "rules": {
         "eqeqeq": 2, //必须使用 === 和 !==
@@ -21,10 +28,11 @@ module.exports = {
         "no-trailing-spaces": 2, //禁止禁用行尾空格
         "space-infix-ops": 2, // 要求操作符周围有空格
         "space-in-parens": 2, //强制在圆括号内使用一致的空格
-        "no-var":2, //要求使用 let 或 const 而不是 var,
-        "no-unused-vars": 1, //禁止出现未使用过的变量
+        "no-var": 2, //要求使用 let 或 const 而不是 var,
         "react/prop-types": 1, //防止在react组件定义中缺少props验证
         "no-mixed-spaces-and-tabs": 2, // 不允许tab和空格混用
         "object-curly-spacing": ['error', 'always'],
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
     }
 };
